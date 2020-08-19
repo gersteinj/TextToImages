08c234e97f53517c56d78c59cb6753e387c85a68
Jackie <gersteinj@gmail.com>
2020-08-18 20:46:17 -0400

    can preview word in plain text

--- js/index.js
+++ js/index.js
@@ -1,39 +1,17 @@
-/////////////////////////////////////////// Drawing ///////////////////////////////////////////
-
-var word = "abcdefg";
-
-// canvas settings
-var padding = 50;
-var font = '60px San_Diego_Font';
-
-// load canvas & context
-var canvas = document.getElementById('main');
-var ctx = canvas.getContext('2d');
-
-// set font so I can resize
-ctx.font = font;
-
-// text sizing
-var textSizing = ctx.measureText(word);
-console.log(textSizing);
-var textWidth = Math.floor(textSizing['width']);
-var textHeight = Math.floor(textSizing['actualBoundingBoxAscent'] + textSizing['actualBoundingBoxDescent']);
-console.log(`text size: ${textWidth} x ${textHeight}`);
-
-// set canvas size
-canvas.width = textWidth + padding;
-canvas.height = textHeight + padding;
-
-// now do text settings, because they get cleared with resize
-ctx.font = font;
-ctx.textAlign = 'center';
-ctx.textBaseline = 'middle';
-
-console.log(`new canvas size: ${canvas.width} x ${canvas.height}`);
-
-// draw text
-ctx.fillColor = 'black';
-ctx.fillText(word, canvas.width/2, canvas.height/2);
-
-
-//////////////////////////////////////// Interactivity ////////////////////////////////////////
+const app = new Vue({
+    el: '#word-preview',
+    data: {
+        // instructionString: 'Enter some text',
+        textString: '',
+        charactersRemaining: 50,
+        foreground: 'black',
+        background: 'red',
+        textSize: 30,
+        textFont: 'serif'
+    },
+    methods: {
+        countCharacters: function() {
+            this.charactersRemaining = 50 - this.textString.length;
+        }
+    }
+})
