6a8594351b61a64e7601f9c4b68586d4e9f71236
Jackie <gersteinj@gmail.com>
2020-08-27 22:12:19 -0400

    preview in vanilla JS

--- app/draw.js
+++ app/draw.js
@@ -1,21 +1,37 @@
 define(function (require) {
-    
+
     var canvData = require('./canvSetup');
-    
+
     var canv = canvData.canvas;
     var ctx = canvData.context;
 
+    function drawBackground() {
+        // draw background
+        ctx.fillStyle = canvData.bgColor;
+        ctx.fillRect(0, 0, canv.width, canv.height);
+    }
+
+    function drawText() {
+        // draw text
+        ctx.fillStyle = canvData.textColor;
+        ctx.fillText(canvData.textString, canv.width / 2, canv.height / 2);
+    }
+
     canvData.textSetup();
     canvData.canvasResize();
-    
-    // draw background
-    ctx.fillStyle = canvData.bgColor;
-    ctx.fillRect(0, 0, canv.width, canv.height);
 
-    // draw text
-    ctx.fillStyle = canvData.textColor;
-    ctx.fillText(canvData.textString, canv.width/2, canv.height/2);
+    drawBackground();
+    drawText();
+
     console.log(canvData.textString);
-});    
+
+    document.getElementById('word').addEventListener('input', function () {
+        console.log(this.value);
+        canvData.textString = this.value;
+        canvData.canvasResize();
+        drawBackground();
+        drawText();
+    });
+});
 
 
